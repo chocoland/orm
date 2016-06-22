25ca1c60b9c1621bb1eff711e66b3fcaa253f0f6
jefer <jdefreitaspinto@gmail.com>
2016-06-21 23:59:57 -0400

    fix changues of lib/choco to vendor/chocoland/orm

--- orm.php
+++ orm.php
@@ -1,13 +1,13 @@
 <?php
 namespace Choco;
-require_once __DIR__ . '/../../vendor/autoload.php';
+//require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/src/yaml.php';
 require_once __DIR__ . '/src/factory.php';
 
 Class ORM {
 	public static function start() {
-		Self::$config = Yaml::read(__DIR__. "/../../" . 'choco.yml');
-		$filenames = scandir(__DIR__. "/../../" . Self::$config_path . 'entities/');
+		Self::$config = Yaml::read(__DIR__. "/../../../" . 'choco.yml');
+		$filenames = scandir(__DIR__. "/../../../" . Self::$config_path . 'entities/');
 		$num = count($filenames);
 		for ($i = 2; $i < $num; $i++) { 
 			array_push(Self::$entities, $filenames[$i]);
